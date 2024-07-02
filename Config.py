38c4f8ece33ff6070802c3b34ce8a7130ed85878
Adithya N <nadithya666@gmail.com>
2024-07-02 17:39:00 +0530

    changing Config

--- Config.py
+++ Config.py
@@ -1,11 +1,6 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    database_name: str
-    database_username: str
-    database_port: str
-    database_password: str
-    database_hostname: str
     secret_key: str
     time_at_expiration: int
     algorithm: str
