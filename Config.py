b43269cec6cd3eec38664af21c05534089a574c8
Adithya N <nadithya666@gmail.com>
2024-07-10 23:40:54 +0530

    new changes in Config.py

--- Config.py
+++ Config.py
@@ -1,14 +1,15 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    secret_key: str
-    time_at_expiration: int
-    algorithm: str
     database_username: str
     database_password: str
     database_hostname: str
     database_name: str
     database_port: str
+    secret_key: str
+    time_at_expiration: int
+    algorithm: str
+
 
     class Config:
         env_file = '.env'
