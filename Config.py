96c79b380e1eef583a69fedc0c20b91537046c63
Adithya N <nadithya666@gmail.com>
2024-07-10 22:50:00 +0530

    changing .env file for ubuntu deployment

--- Config.py
+++ Config.py
@@ -4,7 +4,11 @@ class Settings(BaseSettings):
     secret_key: str
     time_at_expiration: int
     algorithm: str
-    sqlalchemy_db_url: str
+    database_username: str
+    database_password: str
+    database_hostname: str
+    database_name: str
+    database_port: int
 
     class Config:
         env_file = '.env'
